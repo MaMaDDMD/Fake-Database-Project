c6645589ed752f9ce3b1ed1765b5985064079f91
MaMaDDMD <M.H.damad@chmail.ir>
2023-09-11 15:09:13 +0330

    Scaffolding Database

---
+++ Fake Database Project/Fake Database Project/Model/MobileDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Fake_Database_Project.Model
+{
+    public partial class MobileDbContext : DbContext
+    {
+        public MobileDbContext()
+        {
+        }
+
+        public MobileDbContext(DbContextOptions<MobileDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Mobiles> Mobiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-U9P7J2P;Database=Mobile Db;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Mobiles>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.BrandName)
+                    .IsRequired()
+                    .HasColumnType("text");
+
+                entity.Property(e => e.CreatedAt)
+                    .IsRequired()
+                    .HasColumnName("Created_at")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnType("text");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Fake Database Project/Fake Database Project/Model/Mobiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Fake_Database_Project.Model
+{
+    public partial class Mobiles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
