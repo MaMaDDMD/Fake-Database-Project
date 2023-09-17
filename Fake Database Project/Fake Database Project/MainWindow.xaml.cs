49d4083b85507f615c22e3b10d6a2920bf11c539
MaMaDDMD <M.H.damad@chmail.ir>
2023-09-17 17:09:15 +0330

    Handle Process Freezing Bog

--- Fake Database Project/Fake Database Project/MainWindow.xaml.cs
+++ Fake Database Project/Fake Database Project/MainWindow.xaml.cs
@@ -23,24 +23,20 @@ namespace Fake_Database_Project
         private async void SearchBox_Loaded(object sender, RoutedEventArgs e)
         {
             DataProcessing.Visibility = Visibility.Visible;
-            await Task.Delay(2000);
-            await viewmodel.LoadData(Db.Mobiles.ToList());
-            await Task.Delay(2000);
+            await Task.Run(() => viewmodel.LoadData(Db.Mobiles.ToList()));
             DataProcessing.Visibility = Visibility.Collapsed;
         }
 
         private async void TextBox_KeyUp(object sender, KeyEventArgs e)
         {
             QueryProcessing.Visibility = Visibility.Visible;
+
             string SearchStr = SearchBox.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Aggregate("", (s1, s2) => s1 + s2, s => s.Trim().ToLower());
-            await Task.Delay(2000);
-            await viewmodel.LoadData(Db.Mobiles.Where(delegate (Mobiles m)
+            await Task.Run(()=> viewmodel.LoadData(Db.Mobiles.Where(delegate (Mobiles m)
             {
                 string name = m.Name.Split(' ', StringSplitOptions.RemoveEmptyEntries).Aggregate("", (s1, s2) => s1 + s2, s => s.ToLower());
-                string brandname = m.BrandName.Split(' ', StringSplitOptions.RemoveEmptyEntries).Aggregate("", (s1, s2) => s1 + s2, s => s.ToLower());
-                return name.Contains(SearchStr) || brandname.Contains(SearchStr);
-            }).ToList());
-            await Task.Delay(2000);
+                return name.Contains(SearchStr);
+            }).ToList()));
             QueryProcessing.Visibility = Visibility.Collapsed;
         }
     }
--- Fake Database Project/Fake Database Project/ViewModel/DataViewModel.cs
+++ Fake Database Project/Fake Database Project/ViewModel/DataViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 using Fake_Database_Project.Model;
 
 namespace Fake_Database_Project.ViewModel
@@ -8,13 +11,15 @@ namespace Fake_Database_Project.ViewModel
     public class DataViewModel
     {
         public ObservableCollection<Mobiles> Data { get; } = new ObservableCollection<Mobiles>();
-        public Task LoadData(List<Mobiles> mobiles)
+        public async Task LoadData(List<Mobiles> mobiles)
         {
-            Data.Clear();
-            if (mobiles != null)
-                foreach (var item in mobiles)
-                    Data.Add(item);
-            return Task.CompletedTask;
+            await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => Data.Clear()));
+            await Task.Run(() =>
+            {
+                if (mobiles != null)
+                    foreach (var item in mobiles)
+                        Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => Data.Add(item)));
+            });
         }
     }
 }
